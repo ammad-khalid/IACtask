a463a95bdcede7e1f98ce81f37fcfe1173307f5c
ammad-khalid <55195052+ammad-khalid@users.noreply.github.com>
2022-11-23 13:49:33 +0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "prod-vpc" {
 resource "aws_subnet" "prod-subnet-public-1" {
     vpc_id = "${aws_vpc.prod-vpc.id}"
     cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = "true" //it makes this a public subnet
+    map_public_ip_on_launch = "true" 
     availability_zone = "eu-west-2a"
 
     tags = {
@@ -23,7 +23,7 @@ resource "aws_subnet" "prod-subnet-public-1" {
 resource "aws_subnet" "prod-subnet-priv-1" {
     vpc_id = "${aws_vpc.prod-vpc.id}"
     cidr_block = "10.0.2.0/24"
-    map_public_ip_on_launch = "true" //it makes this a public subnet
+    map_public_ip_on_launch = "true" 
     availability_zone = "eu-west-2b"
 
     tags = {
